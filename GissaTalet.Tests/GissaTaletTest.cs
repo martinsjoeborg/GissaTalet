275018406b7dd99969d76fd6d409a90885cadada
sabinafilipovic <sabina-filipovic@hotmail.com>
2024-11-06 14:04:52 +0100

    2

--- GissaTalet.Tests/GissaTaletTest.cs
+++ GissaTalet.Tests/GissaTaletTest.cs
@@ -9,25 +9,17 @@ public class UnitTest1
     [Fact]
     public void TestWordSelection()
     {
-<<<<<<< HEAD
-        // Arrange
-=======
         // Arrange  
         var game = new Core.GissaTalet();
->>>>>>> b6056be8c95df169e770b035c1a13da7c59f83ec
 
         int secretnumber = 50;
 
         //Act
 
-<<<<<<< HEAD
-        // Assert
-=======
         //Assert
         game.HandleGuess(50, secretnumber);
 
         game.isGameOver.ShouldBeTrue();
->>>>>>> b6056be8c95df169e770b035c1a13da7c59f83ec
         
     }
 
